40fc52a4556076c64968b713954a2779e920db67
winston1017 <winston1017@gmail.com>
2017-06-08 19:20:28 -0700

    Healthbar scripted

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private Stat health;
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Q))
+        {
+            health.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            health.CurrentVal += 10;
+        }
+    }
+}
